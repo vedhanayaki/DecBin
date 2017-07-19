bc630486d78aefde95706f3ae4013e76f3c4b7fb
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-19 02:25:18 -0700

    Add files via upload

---
+++ Dec.java
@@ -0,0 +1,16 @@
+package player;
+
+import java.util.Scanner;
+
+public class Dec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the Number");
+		Scanner s=new Scanner(System.in);
+		int a=12;
+		String strBinaryNumber=Integer.toBinaryString(a);
+		System.out.println(strBinaryNumber);
+	}
+
+}
